3d1dda39ec12caa079a1a4a56de1096bb55ad508
Cesar Garcia <elduro2002_@hotmail.com>
2023-12-17 20:50:11 -0400

    add-card

--- src/cards/dto/create-card.dto.ts
+++ src/cards/dto/create-card.dto.ts
@@ -1,1 +1,43 @@
-export class CreateCardDto {}
+import { IsOptional, IsString } from "class-validator";
+
+export class CreateCardDto {
+    @IsString()
+    @IsOptional()
+    card_title?: string;
+  
+    @IsString()
+    @IsOptional()
+    profile_picture?: string;
+  
+    @IsString()
+    @IsOptional()
+    cover_photo?: string;
+  
+    @IsString()
+    @IsOptional()
+    company_logo?: string;
+  
+    @IsString()
+    @IsOptional()
+    name?: string;
+  
+    @IsString()
+    @IsOptional()
+    location?: string;
+  
+    @IsString()
+    @IsOptional()
+    job_title?: string;
+  
+    @IsString()
+    @IsOptional()
+    company?: string;
+  
+    @IsString()
+    @IsOptional()
+    bio?: string;
+  
+    @IsString()
+    @IsOptional()
+    theme_color?: string;
+}
--- src/cards/entities/card.entity.ts
+++ src/cards/entities/card.entity.ts
@@ -1,1 +1,73 @@
-export class Card {}
+import { User } from "src/auth/entities/user.entity";
+import { Column, Entity, ManyToMany, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Content } from "./content.entity";
+
+@Entity('cards')
+export class Card {
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('text', {
+        nullable: true
+    })
+    card_title: string;
+
+
+    @Column('text', {
+        nullable: true
+    })
+    profile_picture: string;
+
+    @Column('text', {
+        nullable: true
+    })
+    cover_photo: string;
+
+    @Column('text', {
+        nullable: true
+    })
+    company_logo: string;
+
+    @Column('text', {
+        nullable: true
+    })
+    name: string;
+
+    @Column('text', {
+        nullable: true
+    })
+    location: string;
+
+    @Column('text', {
+        nullable: true
+    })
+    job_title: string;
+
+    @Column('text', {
+        nullable: true
+    })
+    company: string;
+
+    @Column('text', {
+        nullable: true
+    })
+    bio: string;
+
+    @Column('text', {
+        nullable: true
+    })
+    theme_color: string;
+
+    @ManyToOne(
+        () => User,
+        (user) => user.cards
+    )
+    user: User
+
+    @OneToMany(
+        () => Content,
+        (content) => content.card
+    )
+    contents: Content[]
+}
